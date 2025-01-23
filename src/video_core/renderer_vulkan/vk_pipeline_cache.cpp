2313736d37a1a68937be47abea66329c8a928fd6
Randomuser8219 <168323856+Randomuser8219@users.noreply.github.com>
2025-01-22 20:41:45 -0800

    Update vk_pipeline_cache.cpp

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -226,6 +226,7 @@ bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
     static std::vector<u64> skip_hashes = {
         0x40fb25913cebb787,
         0x8bc545437b881a3d,
+        0xaeb16668b6d39d57,
     };
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
