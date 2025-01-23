a37137b4fd82160a90f973ab1143c9f720be89b7
Randomuser8219 <168323856+Randomuser8219@users.noreply.github.com>
2025-01-22 21:12:38 -0800

    now it's 7

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -228,6 +228,8 @@ bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
         0x8bc545437b881a3d,
         0xaeb16668b6d39d57,
         0xaeb166682bae95e1,
+        0xaeb16668a415ee41,
+        0x8bc54543bb89d540
     };
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
